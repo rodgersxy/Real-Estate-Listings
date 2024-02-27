642cf6acdb9134dd3ffe1ec6c12e246a20f544f8
rodgersxy <rodgersny99@gmail.com>
2024-02-27 13:38:02 +0300

    update/ added img auto slide

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,7 +1,20 @@
-const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
-
-setTimeout(function() {
-    $('#message').fadeOut('slow');
-
-}, 3000);
+document.addEventListener('DOMContentLoaded', function () {
+    const date = new Date();
+    document.querySelector('.year').innerHTML = date.getFullYear();
+  
+    setTimeout(function () {
+      $('#message').fadeOut('slow');
+    }, 3000);
+  
+    // Activate the carousel
+    $('#imageSlider').carousel();
+  
+    document.getElementById('btn-back-to-top').addEventListener('click', function () {
+      // Smoothly scroll to the top of the page
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
